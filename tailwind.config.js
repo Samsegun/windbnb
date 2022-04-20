ccbaf45620ea2b6321b22bf4adc58e787223a0d8
Samsegun <oyebadesamsegun@gmail.com>
2022-04-20 16:04:20 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      mulish: ["Mulish", "serif"],
+      poppins: ["Poppins", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
+// font-family: 'Montserrat', sans-serif;
+// font-family: 'Mulish', sans-serif;
+// font-family: 'Poppins', sans-serif;
